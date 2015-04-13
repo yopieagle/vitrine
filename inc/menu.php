98dfc4a4a87f31b0f05d4d59ddf4dc47a7eed72c
youssefhad <youssefhad2@gmail.com>
2015-04-13 14:46:39 +0100

    first include file for menu

---
+++ inc/menu.php
@@ -0,0 +1,8 @@
+<li class="current-page-item"><a href="#home">Accueil</a></li>
+<li><a href="#about">À Propos</a></li>
+<li><a href="#services">Services</a></li>
+<li><a href="#projects">Projets</a></li>
+<li><a href="#team">Team</a></li>
+<li><a href="#blog">Blog</a></li>
+<li><a href="#clients">Clients</a></li>
+<li><a href="#contacts">Contact</a></li>
